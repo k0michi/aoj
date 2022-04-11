39ea8826393c0a469884e67d9634f4d9dc888bc3
Komichi <k0michi@koyomi.co>
2022-04-11 23:07:27 +0900

    Add 0007

---
+++ 0007.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  int n;
+  std::cin >> n;
+
+  int debt = 100000;
+
+  for (int i = 0; i < n; i++) {
+    debt *= 1.05;
+
+    if (debt % 1000 > 0) {
+      debt += 1000 - debt % 1000;
+    }
+  }
+
+  std::cout << debt << std::endl;
+  return 0;
+}
