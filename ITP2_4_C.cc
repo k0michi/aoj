256e165ae2f392e9ea6f15ddfe39c009c659d842
Komichi <k0michi@koyomi.co>
2023-12-08 17:37:14 +0900

    Add ITP2_4_C

---
+++ ITP2_4_C.cc
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = std::make_signed<size_t>::type;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+
+int main() {
+  i32 n;
+  std::cin >> n;
+  std::vector<i32> a(n);
+
+  for (i32 i = 0; i < n; i++) {
+    std::cin >> a[i];
+  }
+
+  i32 q;
+  std::cin >> q;
+
+  for (i32 i = 0; i < q; i++) {
+    i32 b, e, t;
+    std::cin >> b >> e >> t;
+
+    for (i32 j = 0; j < e - b; j++) {
+      std::swap(a[b + j], a[t + j]);
+    }
+  }
+
+  for (auto it = a.begin(); it != a.end(); it++) {
+    if (it != a.begin()) {
+      std::cout << ' ';
+    }
+
+    std::cout << *it;
+  }
+
+  std::cout << std::endl;
+
+  return 0;
+}
