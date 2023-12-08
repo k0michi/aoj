cdfe2d5fd462d320499021f5e025f7ec4a6457a4
Komichi <k0michi@koyomi.co>
2023-12-08 14:51:43 +0900

    Add ITP2_1_C_stl

---
+++ ITP2_1_C_stl.cc
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = std::make_signed<size_t>::type;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+
+int main() {
+  std::list<i32> list;
+  i32 q;
+  std::cin >> q;
+  auto cursor = list.end();
+
+  for (i32 i = 0; i < q; i++) {
+    i32 op;
+    std::cin >> op;
+
+    if (op == 0) {
+      i32 x;
+      std::cin >> x;
+      cursor = list.insert(cursor, x);
+    } else if (op == 1) {
+      i32 d;
+      std::cin >> d;
+      std::advance(cursor, d);
+    } else if (op == 2) {
+      cursor = list.erase(cursor);
+    }
+  }
+
+  for (auto e : list) {
+    std::cout << e << std::endl;
+  }
+
+  return 0;
+}
