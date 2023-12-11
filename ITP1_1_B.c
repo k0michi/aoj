ccb9c2afede0904987fe57fc82c4e30a00cc0a7f
Komichi <k0michi@koyomi.co>
2023-12-11 18:02:20 +0900

    Add ITP1_1_B.c

---
+++ ITP1_1_B.c
@@ -0,0 +1,27 @@
+#include <ctype.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef size_t usize;
+
+int main() {
+  i32 x;
+  scanf("%d", &x);
+  printf("%d\n", x * x * x);
+  return 0;
+}
