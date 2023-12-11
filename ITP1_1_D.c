46273a2270c853a3f2c74ecd7aa57c0db7a89dce
Komichi <k0michi@koyomi.co>
2023-12-11 18:08:55 +0900

    Add ITP1_1_D.c

---
+++ ITP1_1_D.c
@@ -0,0 +1,33 @@
+#include <ctype.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef size_t usize;
+
+int main() {
+  i32 S;
+  scanf("%d", &S);
+  i32 h, m, s;
+  h = S / (60 * 60);
+  S -= h * (60 * 60);
+  m = S / 60;
+  S -= m * 60;
+  s = S;
+  printf("%d:%d:%d\n", h, m, s);
+  return 0;
+}
