c61598673a3f5512b8d9942abfd33ff3b2132ee7
Komichi <k0michi@koyomi.co>
2023-12-12 12:30:24 +0900

    Add ITP2_10_B

---
+++ ITP2_10_B.cc
@@ -0,0 +1,54 @@
+// Begin preamble
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = std::make_signed<size_t>::type;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float;
+using f64 = double;
+
+// End preamble
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  u32 a, b;
+  std::cin >> a >> b;
+
+  std::cout << std::bitset<32>(a & b) << std::endl;
+  std::cout << std::bitset<32>(a | b) << std::endl;
+  std::cout << std::bitset<32>(a ^ b) << std::endl;
+
+  return 0;
+}
