1bdd1319a17803ff2501728d234131bcf4bfca30
Komichi <k0michi@koyomi.co>
2023-12-12 18:21:24 +0900

    Add NTL_1_A.cc

---
+++ NTL_1_A.cc
@@ -0,0 +1,96 @@
+// Preamble begin
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstring>
+#include <deque>
+#include <forward_list>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <variant>
+#include <vector>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = std::make_signed<size_t>::type;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float;
+using f64 = double;
+
+// Preamble end
+
+bool isPrimeNumber(int x) {
+  if (x == 2) {
+    return true;
+  }
+
+  if (x < 2 || x % 2 == 0) {
+    return false;
+  }
+
+  int sqrtX = static_cast<int>(std::sqrt(x));
+
+  for (int i = 3; i <= sqrtX; i += 2) {
+    if (x % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  i32 n;
+  std::cin >> n;
+
+  std::cout << n << ':';
+
+  if (isPrimeNumber(n)) {
+    std::cout << ' ' << n;
+  } else {
+    for (i32 i = 2; i <= n; i++) {
+      while (n % i == 0) {
+        std::cout << ' ' << i;
+        n /= i;
+      }
+
+      if (n == 1) {
+        break;
+      }
+    }
+  }
+
+  std::cout << std::endl;
+
+  return 0;
+}
