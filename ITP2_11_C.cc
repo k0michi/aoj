3a9e5284bd8ae90cfdf0444d40b82e9ff5023638
Komichi <k0michi@koyomi.co>
2023-12-12 14:50:06 +0900

    Add ITP2_11_C.cc

---
+++ ITP2_11_C.cc
@@ -0,0 +1,85 @@
+// Begin preamble
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = std::make_signed<size_t>::type;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float;
+using f64 = double;
+
+// End preamble
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  i32 n;
+  std::cin >> n;
+  i32 k;
+  std::cin >> k;
+  std::vector<i32> b(k);
+
+  for (i32 i = 0; i < k; i++) {
+    std::cin >> b[i];
+  }
+
+  std::sort(b.begin(), b.end());
+
+  i32 twoPowN = 1 << k;
+
+  for (i32 i = 0; i < twoPowN; i++) {
+    i32 v = 0;
+
+    for (i32 j = 0; j < k; j++) {
+      i32 twoPowJ = 1 << j;
+
+      if ((i & twoPowJ) != 0) {
+        v += (1 << b[j]);
+      }
+    }
+
+    std::cout << v << ":";
+
+    for (i32 j = 0; j < k; j++) {
+      i32 twoPowJ = 1 << j;
+
+      if ((i & twoPowJ) != 0) {
+        std::cout << ' ' << b[j];
+      }
+    }
+
+    std::cout << std::endl;
+  }
+
+  return 0;
+}
