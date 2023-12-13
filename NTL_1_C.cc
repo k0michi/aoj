803b62e6b224eba38fc3599f94c9a8cd21789390
Komichi <k0michi@koyomi.co>
2023-12-13 13:13:25 +0900

    Add NTL_1_C.cc

---
+++ NTL_1_C.cc
@@ -0,0 +1,66 @@
+// Preamble begin
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstring>
+#include <deque>
+#include <forward_list>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <variant>
+#include <vector>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = std::make_signed<size_t>::type;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float;
+using f64 = double;
+
+// Preamble end
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  i32 n;
+  std::cin >> n;
+  i32 result = 1;
+
+  for (i32 i = 0; i < n; i++) {
+    i32 a;
+    std::cin >> a;
+    result = std::lcm(result, a);
+  }
+
+  std::cout << result << std::endl;
+
+  return 0;
+}
