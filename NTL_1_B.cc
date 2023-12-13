af0113b22a59a10cb8510f55ebae7ab94f3e0358
Komichi <k0michi@koyomi.co>
2023-12-13 13:08:51 +0900

    Add NTL_1_B.cc

---
+++ NTL_1_B.cc
@@ -0,0 +1,88 @@
+// Preamble begin
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstring>
+#include <deque>
+#include <forward_list>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <variant>
+#include <vector>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = std::make_signed<size_t>::type;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float;
+using f64 = double;
+
+// Preamble end
+
+constexpr u64 kBase = 1000000007;
+
+std::unordered_map<u64, u64> memo;
+
+u64 m;
+
+u64 powMod(u64 n) {
+  if (n == 0) {
+    return 1;
+  }
+
+  if (memo.count(n) != 0) {
+    return memo[n];
+  }
+
+  u64 result;
+
+  if (n % 2 == 0) {
+    u64 mPowN2 = powMod(n / 2);
+    result = (mPowN2 * mPowN2) % kBase;
+  } else {
+    u64 mPowNMinus1 = powMod(n - 1);
+    result = (mPowNMinus1 * m) % kBase;
+  }
+
+  memo[n] = result;
+  return result;
+}
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  i32 n;
+  std::cin >> m >> n;
+
+  std::cout << powMod(n) << std::endl;
+
+  return 0;
+}
