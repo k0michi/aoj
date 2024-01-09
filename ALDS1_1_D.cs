a98e75176551f20b322a5d3398126adcb04d6cd1
Komichi <k0michi@koyomi.co>
2024-01-10 01:04:44 +0900

    Add ALDS1_1_D.cs

---
+++ ALDS1_1_D.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Text;
+
+// Preamble begin
+
+class ConsoleScanner {
+  private static int charBuf = -1;
+  private static string tokenBuf = null;
+
+  private static int PeekNextChar() {
+    if (charBuf == -1) {
+      charBuf = Console.Read();
+    }
+
+    return charBuf;
+  }
+
+  private static int ReadNextChar() {
+    if (charBuf != -1) {
+      int temp = charBuf;
+      charBuf = -1;
+      return temp;
+    }
+
+    return Console.Read();
+  }
+
+  private static string ConsumeBuffer() {
+    string result = "";
+
+    if (tokenBuf != null) {
+      result += tokenBuf;
+      tokenBuf = null;
+    }
+
+    if (charBuf != -1) {
+      result += charBuf;
+      charBuf = -1;
+    }
+
+    return result;
+  }
+
+  private static void SkipWhitespaces() {
+    while (PeekNextChar() != -1 && char.IsWhiteSpace((char)PeekNextChar())) {
+      ReadNextChar();
+    }
+  }
+
+  private static string ReadNext() {
+    SkipWhitespaces();
+    StringBuilder sb = new StringBuilder();
+
+    if (PeekNextChar() == -1) {
+      return null;
+    }
+
+    while (true) {
+      int next = PeekNextChar();
+
+      if (next == -1) {
+        break;
+      }
+
+      char nextChar = (char)next;
+
+      if (char.IsWhiteSpace(nextChar)) {
+        break;
+      }
+
+      ReadNextChar();
+      sb.Append(nextChar);
+    }
+
+    return sb.ToString();
+  }
+
+  public static string Next() {
+    if (tokenBuf != null) {
+      string temp = tokenBuf;
+      tokenBuf = null;
+      return temp;
+    }
+
+    string token = ReadNext();
+
+    if (token == null) {
+      throw new Exception("No token");
+    }
+
+    return token;
+  }
+
+  private static string Peek() {
+    if (tokenBuf == null) {
+      tokenBuf = ReadNext();
+    }
+
+    return tokenBuf;
+  }
+
+  public static bool HasNext() {
+    if (tokenBuf != null) {
+      return true;
+    }
+
+    SkipWhitespaces();
+    return PeekNextChar() != -1;
+  }
+
+  public static string NextLine() {
+    return ConsumeBuffer() + Console.ReadLine();
+  }
+
+  public static bool HasNextLine() {
+    return PeekNextChar() != -1;
+  }
+
+  public static bool HasNextInt() {
+    string token = Peek();
+    tokenBuf = token;
+    return int.TryParse(token, out _);
+  }
+
+  public static int NextInt() {
+    string token = Next();
+    return int.Parse(token);
+  }
+}
+
+// Preamble end
+
+class Program {
+  static void Main(string[] args) {
+    int n = ConsoleScanner.NextInt();
+    int r = ConsoleScanner.NextInt();
+
+    int minR = r;
+    int maxProfit = int.MinValue;
+
+    for (int i = 1; i < n; ++i) {
+      r = ConsoleScanner.NextInt();
+
+      maxProfit = Math.Max(maxProfit, r - minR);
+      minR = Math.Min(minR, r);
+    }
+
+    Console.WriteLine(maxProfit);
+  }
+}
